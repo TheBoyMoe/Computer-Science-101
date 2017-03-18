08a9533a95d59562f96e46342cb8330cc862db45
bill fero <billfero@gmail.com>
2017-03-18 17:36:58 +0000

    impl fibonacci solution using recursion

--- 01-theory/02-recursion-examples.js
+++ 01-theory/02-recursion-examples.js
@@ -62,3 +62,16 @@ function towerOfHanoi(n, from, to, spare) {
 	return count;
 }
 
+
+/*
+	Using recursion to solve Fibonacci number
+	 
+	 - assumes n is a integer >= 0
+	 - uses two base cases
+
+ */
+function fib(n) {
+	if(n == 0 || n == 1) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
