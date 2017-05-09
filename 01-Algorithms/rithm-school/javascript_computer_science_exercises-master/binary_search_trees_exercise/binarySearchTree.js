072f91d041ad558759f456feb0644ef9d853cd01
bill fero <billfero@gmail.com>
2017-05-09 14:15:49 +0100

    impl find iteratively and recursively

---
+++ 01-Algorithms/rithm-school/javascript_computer_science_exercises-master/binary_search_trees_exercise/binarySearchTree.js
@@ -0,0 +1,109 @@
+/* Binary Search Tree Implementation */
+'use strict';
+
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right  = null;
+}
+
+function BinarySearchTree(comparator = null) {
+    this.root = null;
+    this.comparator = comparator;
+    this.compare = function (left, right) {
+        if(this.comparator !== null)
+            return this.comparator(left, right);
+        
+        return (left < right)? -1: (right < left)? 1: 0;
+    }
+}
+
+BinarySearchTree.prototype.insertIteratively = function (value) {
+    let node = new Node(value);
+    if(this.root === null) {
+        this.root = node;
+        return this;
+    }
+    else {
+        let current = this.root;
+        while(true) {
+            if (value < current.value) {
+                if(current.left === null){
+                    current.left = node;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            } else if(value > current.value){
+                if(current.right === null){
+                    current.right = node;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+    }
+};
+
+BinarySearchTree.prototype.insertRecursively = function (value) {
+    if(this.root = null){
+        this.root = new Node(value);
+        return this;
+    }
+    return this.__insert(value);
+};
+
+
+// return the node if found, otherwise return undefined
+BinarySearchTree.prototype.findIteratively = function (value) {
+    let current = this.root, found = false;
+    if(current.value === value) return current;
+    while(current && !found){
+        if(value < current.value){
+            current = current.left;
+        } else if(value > current.value){
+            current = current.right;
+        } else {
+            found = true;
+        }
+    }
+    if(!found) return undefined;
+    
+    return current;
+};
+
+BinarySearchTree.prototype.findRecursively = function (value) {
+    return this.__find(value);
+}
+
+
+
+// HELPER FUNCTIONS
+BinarySearchTree.prototype.__insert = function (value, node = this.root) {
+    if(node === null) return this;
+    
+    if(this.compare(value, node.value) <= 0){
+        if(node.left === null){
+            node.left = new Node(value);
+            return this;
+        }
+        return this.__insert(value, node.left);
+    }
+    if(node.right === null){
+        node.right = new Node(value);
+        return this;
+    }
+    return this.__insert(value, node.right);
+};
+
+BinarySearchTree.prototype.__find = function (value, node = this.root, parent = null) {
+    if(!node) return undefined;
+    
+    if(this.compare(value, node.value) === 0) return node;
+    
+    if(this.compare(value, node.value) < 0)
+        return this.__find(value, node.left, node);
+    
+    return this.__find(value, node.right, node);
+};
